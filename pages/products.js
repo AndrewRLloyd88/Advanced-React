adc34ecb073cb6450b7fe7b69286d83971aa5a23
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-04-26 17:42:57 -0700

    work on setup for pages

---
+++ pages/products.js
@@ -0,0 +1,7 @@
+export default function ProductPage() {
+  return (
+    <div>
+      <p>Hey This is Products</p>
+    </div>
+  );
+}
---
+++ pages/sell.js
@@ -0,0 +1,11 @@
+import Page from '../components/Page';
+
+export default function SellPage() {
+  return (
+    <Page>
+      <div>
+        <p> I am the sell Page</p>
+      </div>
+    </Page>
+  );
+}
