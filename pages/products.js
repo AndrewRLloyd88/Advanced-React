6f68b29a09a4599c4cad5690ff8e6f2858e2de4a
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-05-19 14:21:23 -0700

    add pagination ui to products

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,12 @@
 import Products from '../components/Products';
+import Pagination from '../components/Pagination';
 
 export default function ProductPage() {
   return (
     <div>
+      <Pagination page={10} />
       <Products />
+      <Pagination page={1} />
     </div>
   );
 }
